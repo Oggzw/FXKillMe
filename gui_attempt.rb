7847a0ad43a813b73960c7c9f42deeb4a2586902
Oggzw <44577605+Oggzw@users.noreply.github.com>
2020-03-30 00:24:50 +0200

    Update gui_attempt.rb

--- gui_attempt.rb
+++ gui_attempt.rb
@@ -1,149 +1,69 @@
-
 require 'fox16'
-
 include Fox
-
 class TreeListExample < FXMainWindow
   def initialize(app)
-    super(app, "My Tunes", :width => 400, :height => 200)
-    treelist_frame = FXHorizontalFrame.new(self, FRAME_SUNKEN|FRAME_THICK|LAYOUT_FILL)
+  @Hash = {}
+  super(app, "Directory", :width => 400, :height => 200)
+	treelist_frame = FXHorizontalFrame.new(self, FRAME_SUNKEN|FRAME_THICK|LAYOUT_FILL)
+  treelist = FXTreeList.new(treelist_frame,
+	  :opts => TREELIST_NORMAL|TREELIST_SHOWS_LINES| \
+			   TREELIST_SHOWS_BOXES|TREELIST_ROOT_BOXES|LAYOUT_FILL)
 
-    bigfolder = FXPNGIcon.new(app, File.open("minifolder.png", "rb").read)
-    fileIcon = FXPNGIcon.new(app, File.open("minidoc.png", "rb").read)
+	bigfolder = FXPNGIcon.new(app, File.open("minifolder.png", "rb").read)
+  fileIcon = FXPNGIcon.new(app, File.open("minidoc.png", "rb").read)
+	
+	wd = Dir.pwd
+	dir = Dir.glob('**/*', base:wd)
+	dir_is_dir = []
+	dir_is_file = []
+	dir.each do |dir|
+	  if File.directory?(dir) == true
+		dir_is_dir << dir
+
+	  end
+	  if File.file?(dir) == true
+		dir_is_file << dir
+	  end
+	end
 
 
+  threadCreation = 0 
+  
+  dir_is_dir.each do | dir |
+    if dir.include?("/") == true
 
+      #One-liner kek> Skapar en ny key i hash @Hash med värdet av directory. Denna key innehåller en instance variable set som ökar
+      #Denna variable innehåller ett treelistItem. Den första partition gör så att den hamnar i rätt instance variable som skapas under i
+      #som skapas under i elsif.
+     
+      @Hash[instance_variable_set("@H" + "#{threadCreation}", treelist.appendItem(@Hash.key(dir.rpartition("/")[0]), dir.rpartition("/")[-1]))] = dir
+    elsif
+      @Hash[instance_variable_set("@H" + "#{threadCreation}", treelist.appendItem(nil, dir))] = dir 
 
-    treelist = FXTreeList.new(treelist_frame,
-      :opts => TREELIST_NORMAL|TREELIST_SHOWS_LINES| \
-               TREELIST_SHOWS_BOXES|TREELIST_ROOT_BOXES|LAYOUT_FILL)
-
-
-
-
-
-
-    wd = Dir.pwd
-    dir = Dir.glob('**/*', base:wd)
-    dir_is_dir = []
-    dir_is_file = []
-    dir.each do |dir|
-      if File.directory?(dir) == true
-        dir_is_dir << dir
-      end
-      if File.file?(dir) == true
-        dir_is_file << dir
-      end
+    end 
+    treelist.setItemOpenIcon(@Hash.key(dir), bigfolder)
+    treelist.setItemClosedIcon(@Hash.key(dir), bigfolder)
+    threadCreation = threadCreation + 1
+  end
+  dir_is_file.each do | dir |
+    if dir.include?("/") == true
+      treelist.appendItem(@Hash.key(dir.rpartition("/")[0]), dir.rpartition("/")[-1], oi = fileIcon,ci = fileIcon)
+    elsif
+      treelist.appendItem(nil, dir.rpartition("/")[-1], oi = fileIcon,ci = fileIcon)
     end
-
-   
-   
-    dir_is_file = dir.map { |file| [file.count("/"), file] }  # KEK
-
-
-    dir = dir.map { |file| [file.count("/"), file] }
-
-
-    
-
-    i = 0 # amount of / : or the count of children from base folder.
-    
-    dir_is_dir.each do |dir|
-      dir = dir.gsub(" ", "_")
-      
-
-
-
-
-
-    end
-
-
-    
-
-
-
-   
-
-
-    dir.each do |count, dir|
-      dir = dir.gsub(" ", "_")
-      
-
-      if i == 0
-        if File.directory?(dir) == true
-         # instance_variable_set('@'+dir.gsub(" ", "_"), treelist.prependItem(nil, dir))
-         # treelist.setItemClosedIcon(@dir, bigfolder)
-         # treelist.setItemOpenIcon(@dir, bigfolder)
-
-        elsif File.file?(dir) == true
-          #treelist.appendItem(nil,dir)
-        end
-      end
-      if i == 1
-        if File.directory?(dir) == true
-
-        elsif File.file?(dir) == true
-          temp = dir.partition("/")[i-1]
-          puts temp
-          
-          treelist.appendItem(@icons, dir, oi = fileIcon,ci = fileIcon)
-
-        end
-      end
-      i = count
-    end
-
-    #artist_1    = treelist.appendItem(nil, "Dir1")
-    #album_1_1   = treelist.prependItem(artist_1, "Every Time You Say Goodbye")
-    #album_1_2   = treelist.appendItem(artist_1, "Dir1/File.txt")
-    #track_1_2_1 = treelist.insertItem(track_1_2_2, album_1_2, "Stay")
-    #track_1_2_2 = treelist.prependItem(album_1_2, "Maybe")
-    #track_1_2_3 = treelist.appendItem(album_1_2, "Ghost in this House")
-    #
-    #
-    #artist_2    = treelist.insertItem(artist_3, nil, "Billy Joel")
-    #album_2_1   = treelist.appendItem(artist_2, "Greatest Hits, Vol. I")
-    #
-    #
-    #artist_3    = treelist.appendItem(nil, "Chicago")
-    #album_3_1   = treelist.appendItem(artist_3, "Greatest Hits, Vol. I")
-    #
-    #
-    #artist_4    = treelist.appendItem(nil, "They Might Be Giants")
-    #album_4_1   = treelist.appendItem(artist_4, "The Else")
-    #
-    #
-    #
-    #
-    #treelist.setItemOpenIcon(track_1_2_3, fileIcon)
-    #treelist.setItemClosedIcon(track_1_2_3, fileIcon)
-    #treelist.setItemClosedIcon(artist_1, bigfolder)
-    #treelist.setItemOpenIcon(artist_1, bigfolder)
-    #
-    
+  end
   end
 
   def create
-    super
-    show(PLACEMENT_SCREEN)
+	super
+	show(PLACEMENT_SCREEN)
   end
 end
 
-
-
-
-
-
-
-
-
 if __FILE__ == $0
   FXApp.new do |app|
-    TreeListExample.new(app)
-    app.create
-
-    app.run
-
+	TreeListExample.new(app)
+	app.create
+	app.run
   end
 end
